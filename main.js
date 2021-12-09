4b1603f8cb7e914a66b92a4596bca03d535aa796
raczylos <sebastian.raczylo@gmail.com>
2021-12-09 10:09:12 +0100

    working editi and remove

--- main.js
+++ main.js
@@ -52,15 +52,21 @@ class Product {
 
     editElement = () => {
 
-
-
         this.nameElement.innerHTML = this.name
         this.amountElement.innerHTML = this.amount
         this.priceElement.innerHTML = this.price
         this.sumElement.innerHTML = this.sum
+    }
+    removeElement = () => {
+
+        this.element.removeChild(this.nameElement)
+        this.element.removeChild(this.amountElement)
+        this.element.removeChild(this.priceElement)
+        this.element.removeChild(this.sumElement)
 
 
     }
+
 }
 
 class ProductContainer {
@@ -70,59 +76,99 @@ class ProductContainer {
     productsContainerElement = document.getElementsByClassName("productContainer")[0]
 
     AddProduct = (product) => {
-        this.products.push(product)
-        this.productsContainerElement.appendChild(product.createElement())
-        let select1 = document.getElementById("editSelect");
+        if(this.products.find(x => x.name == product.name)){
+            alert("Dodawany przedmiot istnieje już na liście")
+        }
+        else{
 
-        let select2 = document.getElementById("removeSelect");
-
-        let option1 = document.createElement("option");
-
-
-        option1.text = product.name
-
-        let option2 = option1.cloneNode(true)
-
-        select1.add(option1)
-        select2.add(option2)
-
-
-        console.log(this.products)
-
+            if (isNaN(product.name) && product.price && product.amount) {
+    
+                this.products.push(product)
+                this.productsContainerElement.appendChild(product.createElement())
+                let editSelect = document.getElementById("editSelect");
+    
+                let removeSelect = document.getElementById("removeSelect");
+    
+                let option1 = document.createElement("option");
+    
+    
+                option1.text = product.name
+    
+                let option2 = option1.cloneNode(true)
+    
+                editSelect.add(option1)
+                removeSelect.add(option2)
+    
+                console.log(this.products)
+            }
+            else {
+                alert("Upewnij się, że dane przyd dodawaniu produktu zostały wprowadzone prawidłowo!!!")
+            }
+        }
 
     }
 
     RemoveProduct = () => {
 
-        // let select1 = document.getElementById("editSelect");
+        let editSelect = document.getElementById("editSelect");
 
-        // let select2 = document.getElementById("removeSelect");
+        let removeSelect = document.getElementById("removeSelect");
 
-        // select1.remove()
-        // select2.remove()
+        console.log(removeSelect.selectedIndex)
+
+
+        let product = this.products.find(x => x.name == removeSelect.options[removeSelect.selectedIndex].text)
+
+
+        product.removeElement()
+        this.products = this.products.filter(x => x.name != removeSelect.options[removeSelect.selectedIndex].text) // zmien chyba??
 
         console.log(this.products)
 
+        editSelect.remove(removeSelect.selectedIndex)
+        removeSelect.remove(removeSelect.selectedIndex)
 
 
-        // this.products(idx).splice(idx, 1)
+
     }
 
-    EditProduct = (name, price, amount) => {
+    EditProduct = (name, amount, price) => {
         let select = document.getElementById("editSelect");
-        let value = select.options[select.selectedIndex].value;
+        let removeSelect = document.getElementById("removeSelect")
 
-        this.products[select.selectedIndex].name = name
-        this.products[select.selectedIndex].price = price
-        this.products[select.selectedIndex].amount = amount
-        this.products[select.selectedIndex].sum = price * amount
-
-
-        this.products[select.selectedIndex].editElement()
-
-
-
-
+        if(this.products.find(x => x.name == name)){
+            alert("Nie możesz edytować nazwy produktu na tą która już istnieje")
+        }
+        else{
+            if (price != 0 && amount != 0) {
+                if (isNaN(name) && !isNaN(price) && !isNaN(amount)) {
+                    this.products[select.selectedIndex].name = name
+                    this.products[select.selectedIndex].price = price
+                    this.products[select.selectedIndex].amount = amount
+                    this.products[select.selectedIndex].sum = price * amount
+    
+                    this.products[select.selectedIndex].editElement() // akutalizacja wyświetlania
+    
+                    let product = this.products.find(x => x.name == name)
+    
+                    let option1 = document.createElement("option");
+    
+                    option1.text = product.name
+                    let option2 = option1.cloneNode(true)
+    
+    
+                    removeSelect.options[select.selectedIndex] = option2 // akutalizacja selecta edit
+                    select.options[select.selectedIndex] = option1 // akutalizacja selecta remove
+                }
+                else {
+                    alert("Upewnij się, że dane edycji zostały wprowadzone prawidłowo!!!")
+                }
+    
+            }
+            else {
+                alert("Ilość i cena nie mogą być 0!!!")
+            }
+        }
     }
 
     SwapProducts = () => {
